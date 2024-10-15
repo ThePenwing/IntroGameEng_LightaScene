ba5fa36305477bfcd3a07ee6787545028bfe0657
ThePenwing <unitedhayden@outlook.com>
2024-10-15 16:26:00 -0300

    Initial commit

---
+++ Assets/GhostTravel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTravel : MonoBehaviour
+{
+
+    public GameObject ghostWaypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
